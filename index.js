0ff8a9d8a1d9f32dc52bcde7cd3199cbd840bd65
anthony.buchholz <anthony.buchholz@gmail.com>
2019-12-15 15:36:00 -0600

    Added apikey as input from frontend

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 let express = require('express');
 let env = require('node-env-file');
 let app = express();
-let RiotApi = require('./TeemoJS/src/index');
+let TeemoJS = require('./TeemoJS/src/index');
 env('.env');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
-
-RiotApi = RiotApi(process.env.RIOT_API_KEY);
 
 let tournamentProvider = async function(body) {
     try {
+        let RiotApi = TeemoJS(body.apikey);
+        delete body.apikey;
         let response = await RiotApi.req('na', 'tournament.stubV4.registerProviderData', {}, {}, body);
         return {
             providerId: response
         };
     } catch (err) {
-        console.error(err.response);
+        let error = await err.response.text();
+        return JSON.parse(error);
     }
 }
 
 let tournament = async function(body) {
     try {
+        let RiotApi = TeemoJS(body.apikey);
+        delete body.apikey;
         let response = await RiotApi.req('americas', 'tournament.stubV4.registerTournament', {}, {}, body);
         return {
             tournamentId: response
         };
     } catch (err) {
-        console.error(err.response);
+        let error = await err.response.text();
+        return JSON.parse(error);
     }
 }
 
 let tournamentCodes = async function(body) {
     try {
+        let RiotApi = TeemoJS(body.apikey);
         let count  = body.count;
         let tournamentId = body.tournamentId;
-        delete body.count; delete body.tournamentId;
+        delete body.count; delete body.tournamentId; delete body.apikey;
         let response = await RiotApi.req('americas', 'tournament.stubV4.createTournamentCode', {}, {count: count, tournamentId: tournamentId}, body);
         return {
-            tournamentId: response
+            tournamentCodes: response
         };
     } catch (err) {
-        console.error(err.response.body);
+        let error = await err.response.text();
+        return JSON.parse(error);
     }
 }
 
@@ -56,7 +67,6 @@ app.post('/tournament', async function(req, res){
 });
 
 app.post('/tournament-codes', async function(req, res){
-    console.log(req);
     tournamentCodes(req.body)
         .then(result => res.json(result))
         .catch(err => console.error(err));
